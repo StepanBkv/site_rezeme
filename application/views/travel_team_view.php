2fa1a2f0dc4e7e779539e500df6a6b143cd6c524
Степан Бабаков <stepan.babakov.1999@mail.ru>
2022-02-10 23:40:04 +0300

    Добавлена svg файл гор и надписи под svg-шкой

--- application/views/travel_team_view.php
+++ application/views/travel_team_view.php
@@ -42,8 +42,18 @@
 
 							<div class="sect_wrap_block">
 								<div class="sect_wrap_block_d sect_wrap_block_1">
-									
+									<div class="block__content_1">
+										<div class="mount__svg">
+									<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="svg-65a8" x="0px" y="0px" viewBox="0 0 468.293 468.293" style="enable-background:new 0 0 468.293 468.293;" xml:space="preserve" class="u-svg-content"><rect x="19.774" y="320.903" style="fill:#31978C;" width="9.21" height="59.105"></rect><path style="fill:#44C4A1;" d="M0,283.639c0,28.01,10.914,50.717,24.377,50.717V232.922C10.914,232.922,0,255.629,0,283.639z"></path><path style="fill:#3EA69B;" d="M24.377,232.922v101.435c13.463,0,24.377-22.707,24.377-50.717  C48.754,255.629,37.84,232.922,24.377,232.922z"></path><g><circle style="fill:#31978C;" cx="35.34" cy="293.149" r="4.602"></circle><circle style="fill:#31978C;" cx="35.34" cy="274.105" r="4.602"></circle>
+</g><g><circle style="fill:#3EA69B;" cx="12.188" cy="266.113" r="4.602"></circle><circle style="fill:#3EA69B;" cx="12.188" cy="301.191" r="4.602"></circle>
+</g><rect x="432.684" y="307.385" style="fill:#31978C;" width="11.314" height="72.629"></rect><path style="fill:#44C4A1;" d="M408.384,261.591c0,34.419,13.411,62.32,29.954,62.32V199.27  C421.795,199.27,408.384,227.172,408.384,261.591z"></path><path style="fill:#3EA69B;" d="M438.338,199.27v124.641c16.543,0,29.954-27.902,29.954-62.32  C468.293,227.172,454.882,199.27,438.338,199.27z"></path><g><circle style="fill:#31978C;" cx="451.809" cy="273.294" r="5.657"></circle><circle style="fill:#31978C;" cx="451.809" cy="249.891" r="5.657"></circle>
+</g><g><circle style="fill:#3EA69B;" cx="423.337" cy="240.02" r="5.657"></circle><circle style="fill:#3EA69B;" cx="423.337" cy="283.159" r="5.657"></circle>
+</g><polygon style="fill:#DC8744;" points="444.126,380.01 348.833,207.085 334.425,221.845 308.863,195.652 283.305,221.845   257.739,195.652 232.174,221.845 217.848,207.166 122.599,380.01 "></polygon><polygon style="fill:#64798A;" points="283.363,88.278 266.992,117.986 329.739,219.019 411.384,380.01 444.126,380.01 "></polygon><path style="fill:#EBF0F3;" d="M257.739,195.652l20.878,21.391c2.57,2.633,6.804,2.633,9.373-0.001l20.872-21.391l20.876,21.392  c2.569,2.633,6.803,2.633,9.373,0.001l9.722-9.959l-65.47-118.806l-65.515,118.888l9.64,9.877c2.57,2.633,6.803,2.633,9.373,0  L257.739,195.652z"></path><polygon style="fill:#DC8744;" points="270.235,380.01 196.021,245.338 184.8,256.833 164.893,236.434 144.989,256.833   125.079,236.434 105.169,256.833 94.012,245.401 19.833,380.01 "></polygon><polygon style="fill:#64798A;" points="145.034,152.813 132.285,175.949 181.151,254.632 244.736,380.01 270.235,380.01 "></polygon><path style="fill:#EBF0F3;" d="M125.079,236.434l16.26,16.659c2.001,2.051,5.299,2.05,7.3-0.001l16.255-16.659l16.258,16.66  c2.001,2.05,5.298,2.051,7.299,0l7.571-7.756l-50.987-92.525l-51.022,92.588l7.508,7.692c2.001,2.05,5.298,2.05,7.299,0  L125.079,236.434z"></path><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg>
+									</div>
+									<h2 class = "text__h2 text__1">Hike more.</h2>
+									<h2 class = "text__h2 text__2">WORRY LESS.</h2>
 								</div>
+							</div>
 								<div class="sect_wrap_block_d sect_wrap_block_2">
 									
 								</div>
