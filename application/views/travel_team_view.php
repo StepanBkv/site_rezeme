fbd64a14bcb60858a57d543d0c868f8bcf51a75f
Степан Бабаков <stepan.babakov.1999@mail.ru>
2022-02-17 14:27:21 +0300

    1 секция готова, осталась 2-я

--- application/views/travel_team_view.php
+++ application/views/travel_team_view.php
@@ -52,6 +52,18 @@
 									</div>
 									<h2 class = "text__h2 text__1">Hike more.</h2>
 									<h2 class = "text__h2 text__2">WORRY LESS.</h2>
+									<div class="div__socnet">
+											<div class="div__blocknet">
+												<a class="social" target="_blank" href=""><span class="u-icon u-icon-circle u-social-facebook u-social-icon u-icon-2"><svg x="0px" y="0px" viewBox="0 0 112 112" id="svg-0bd7" class="u-svg-content"><path d="M75.5,28.8H65.4c-1.5,0-4,0.9-4,4.3v9.4h13.9l-1.5,15.8H61.4v45.1H42.8V58.3h-8.8V42.4h8.8V32.2 c0-7.4,3.4-18.8,18.8-18.8h13.8v15.4H75.5z"></path></svg></span>
+	                      						</a>
+	                      						<a class="social" target="_blank" href=""><span class="u-icon u-icon-circle u-social-icon u-social-twitter u-icon-3"></svg><svg x="0px" y="0px" viewBox="0 0 112 112" id="svg-4ed7" class="u-svg-content"><path d="M92.2,38.2c0,0.8,0,1.6,0,2.3c0,24.3-18.6,52.4-52.6,52.4c-10.6,0.1-20.2-2.9-28.5-8.2 c1.4,0.2,2.9,0.2,4.4,0.2c8.7,0,16.7-2.9,23-7.9c-8.1-0.2-14.9-5.5-17.3-12.8c1.1,0.2,2.4,0.2,3.4,0.2c1.6,0,3.3-0.2,4.8-0.7 c-8.4-1.6-14.9-9.2-14.9-18c0-0.2,0-0.2,0-0.2c2.5,1.4,5.4,2.2,8.4,2.3c-5-3.3-8.3-8.9-8.3-15.4c0-3.4,1-6.5,2.5-9.2 c9.1,11.1,22.7,18.5,38,19.2c-0.2-1.4-0.4-2.8-0.4-4.3c0.1-10,8.3-18.2,18.5-18.2c5.4,0,10.1,2.2,13.5,5.7c4.3-0.8,8.1-2.3,11.7-4.5 c-1.4,4.3-4.3,7.9-8.1,10.1c3.7-0.4,7.3-1.4,10.6-2.9C98.9,32.3,95.7,35.5,92.2,38.2z"></path></svg></span>
+	                     						</a>
+	                      						<a class="social" target="_blank" href=""><span class="u-icon u-icon-circle u-social-icon u-social-instagram u-icon-4"><svg x="0px" y="0px" viewBox="0 0 112 112" id="svg-1f05" class="u-svg-content"><path d="M55.9,32.9c-12.8,0-23.2,10.4-23.2,23.2s10.4,23.2,23.2,23.2s23.2-10.4,23.2-23.2S68.7,32.9,55.9,32.9z M55.9,69.4c-7.4,0-13.3-6-13.3-13.3c-0.1-7.4,6-13.3,13.3-13.3s13.3,6,13.3,13.3C69.3,63.5,63.3,69.4,55.9,69.4z"></path><path d="M79.7,26.8c-3,0-5.4,2.5-5.4,5.4s2.5,5.4,5.4,5.4c3,0,5.4-2.5,5.4-5.4S82.7,26.8,79.7,26.8z"></path><path d="M78.2,11H33.5C21,11,10.8,21.3,10.8,33.7v44.7c0,12.6,10.2,22.8,22.7,22.8h44.7c12.6,0,22.7-10.2,22.7-22.7 V33.7C100.8,21.1,90.6,11,78.2,11z M91,78.4c0,7.1-5.8,12.8-12.8,12.8H33.5c-7.1,0-12.8-5.8-12.8-12.8V33.7 c0-7.1,5.8-12.8,12.8-12.8h44.7c7.1,0,12.8,5.8,12.8,12.8V78.4z"></path></svg></span>
+	                      						</a>
+	                      						<a class="social" target="_blank" href="#"><span class="u-icon u-icon-circle u-social-icon u-social-youtube u-icon-5"><svg x="0px" y="0px" viewBox="0 0 112 112" id="svg-99ec" class="u-svg-content"><path d="M82.3,24H29.7C19.3,24,11,32.5,11,43v26.7c0,10.5,8.3,19,18.7,19h52.5c10.3,0,18.7-8.5,18.7-19V43 C101,32.5,92.7,24,82.3,24L82.3,24z M69.7,57.6L45.1,69.5c-0.7,0.2-1.4-0.2-1.4-0.8V44.1c0-0.7,0.8-1.3,1.4-0.8l24.6,12.6 C70.4,56.2,70.4,57.3,69.7,57.6L69.7,57.6z"></path></svg></span>
+	                      						</a>
+                      						</div>
+										</div>
 								</div>
 							</div>
 								<div class="sect_wrap_block_d sect_wrap_block_2">
@@ -60,18 +72,44 @@
 								<div class="sect_wrap_block_d sect_wrap_block_3">
 									<div class="block__content">
 										<div class="div_line"></div>
-										<p>This is a majestic landscape of mountains, deep valleys and glaciers, dominated by Mount Everest (otherwise known as Sagarmatha in Nepali) at 8,848m above sea level. </p>
-									</div>
+										<p>This is a majestic landscape of mountains, deep valleys and glaciers, dominated by Mount Everest (otherwise known as Sagarmatha in Nepali) at 8,848m above sea level. 
+										</p>
+										</div>
 								</div>
 							</div>
 						</div>
 					</section>	
-					<section class="sect_2"></section>
-					<section class="sect_3"></section>
-					<section class="sect_4"></section>	
-					<section class="sect_5"></section>
-					<section class="sect_6"></section>
-					<section class="sect_7"></section>
+					<section class="sect__2">
+						<div class="sect__wrap">
+							 <div class="sect_wrap_block">
+
+							 	<div class="sect_wrap_block_d sect_wrap_block_1">
+							 		<div class="block__content">
+							 			<div class="block__img">
+							 				<img src="img/travel_1.jpg">
+							 			</div>
+							 			<div class="block_wrap_text">
+								 			<div class="block__text">
+								 				<h6>01</h6>
+								 				<p>Amet mattis vulputate enim nulla aliquet. Morbi non arcu risus quis varius quam quisque id. Ultrices gravida dictum fusce ut placerat. Sit amet nisl suscipit adipiscing bibendum est ultricies integer imperdiet nulla</p>
+								 			</div>
+							 			</div>
+							 		</div>
+							 	</div>
+							 	<div class="sect_wrap_block_d sect_wrap_block_2">
+							 		
+							 	</div>
+							 	<div class="sect_wrap_block_d sect_wrap_block_3">
+							 		
+							 	</div> 
+							 </div>
+						</div>
+					</section>
+					<section class="sect__3"></section>
+					<section class="sect__4"></section>	
+					<section class="sect__5"></section>
+					<section class="sect__6"></section>
+					<section class="sect__7"></section>
 				</div>
 			</div>
 		</div>
